b974cf41cd45ce03a13003ddb89b217c2ad98d18
Cheolkie Kim <ckiekim@naver.com>
2024-03-29 10:19:39 +0900

    React

--- src/components/User2.jsx
+++ src/components/User2.jsx
@@ -10,15 +10,16 @@ export default function User() {
   const [form, setForm] = useState({id:'', name:'', email:''});
   const handleSubmit = (event) => {
     event.preventDefault();   // submit button을 누르면 페이지가 자동적으로 바뀌는 것을 방지
-    const existUser = users.find(user => user.id == form.id);
-    const newUsers = [];
+    const existUser = users.find(user => user.id === form.id);
+    // const newUsers = [];
     if (existUser) {
-      for (let user of users)
-        if (user.id == form.id)
-          newUsers.push(form);
-        else
-          newUsers.push(user);
-      setUsers(newUsers);
+      setUsers(users.map(user => (user.id === form.id) ? form : user))
+      // for (let user of users)
+      //   if (user.id == form.id)
+      //     newUsers.push(form);
+      //   else
+      //     newUsers.push(user);
+      // setUsers(newUsers);
     } else
       setUsers([...users, form]);
   }
@@ -45,22 +46,16 @@ export default function User() {
       <br />
       <form onSubmit={handleSubmit}>
         <label htmlFor='id'>아이디:</label>
-        <input type='text' id='id' name='id' value={form.id} 
-          onChange={handleChange} 
-        /><br />
+        <input type='text' id='id' name='id' value={form.id} onChange={handleChange} /><br />
         <label htmlFor='name'>이름:</label>
-        <input type='text' id='name' name='name' value={form.name} 
-          onChange={handleChange} 
-        /><br />
+        <input type='text' id='name' name='name' value={form.name} onChange={handleChange} /><br />
         <label htmlFor='email'>이메일:</label>
-        <input type='text' id='email' name='email' value={form.email} 
-          onChange={handleChange} 
-        /><br />
+        <input type='text' id='email' name='email' value={form.email} onChange={handleChange} /><br />
         <button>추가/수정</button>
       </form>
       <button onClick={() => {
         const id = prompt('Id 값을 입력하세요');
-        setUsers(users.filter(user => user.id != id));
+        setUsers(users.filter(user => user.id !== id));
       }}>삭제</button>
     </div>
   );
