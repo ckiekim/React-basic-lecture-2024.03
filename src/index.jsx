d212c7d1738ceb23bdc7d29663e9aa5a3b71ae5b
Cheolkie Kim <ckiekim@naver.com>
2024-04-03 09:31:13 +0900

    React basic

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './apps/App21_router';
+import App from './apps/App99_exam';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
